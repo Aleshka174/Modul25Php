6c0f678a68d477f3f8b2e1114438bbf2c8a3057f
Aleshka174 <alexey20.05@inbox.ru>
2021-02-02 23:22:15 +0500

    second change

--- index.php
+++ index.php
@@ -1,55 +1,90 @@
 <?php 
-	interface familyInterface{
-		public $type = 'Human';
-		public function loveEachOther()
-	    {
-
-	    }
+	interface familyInterface
+	{
+		public function loveEachOther();
 	}
-
 	abstract class family implements familyInterface
 	{
-	    private $peopleCount = 3;
+		 public $type = "Человек";
+		 public $age;
+	    private $peopleCount;
 	    public function teachChild()
 	    {
-	 
-	    }	    
+	 		echo "Учить ребенка";
+	    }	
+	    public function loveEachOther()
+	    {
+	    	echo "Любить друг друга";
+	    }    
 	}
 
-	interface interfaceFarther
+	class Farther extends family
 	{
-		public $intellect = 'clever';
-	}
-	
-	class Farther extends family implements interfaceFarther
-	{
-	    
-		public $age = 30;
+		public $geneticM = 'Ум';
+		public $wife;
 		public function makeMoney()
 		{
-
+			echo "Заработок денег";
+		}
+		public function geneMan()
+		{
+			echo $this->geneticM;
 		}
 	}
-	interface interfaceMother
-		{
-			public $eyes = 'blue';
-		}
-	
-	class Mother extends family implements interfaceMother
+	interface motherInterface
 	{
-		public $age = 27;
+		const geneticW = 'Глаза';
+		public function geneWoman();
+	}
+	class Mother extends family 
+	{
+		public $husband;
+		public $geneticW = 'Глаза';
 		public function cook()
 		{
+			echo "Приготовка еды";
+		}
+		public function geneWoman()
+		{
+			echo $this->geneticW;
+		}
+	}
 
+	class child extends farther implements motherInterface
+	{
+		public $sex;
+		public function teach()
+		{
+			echo "Учиться в Школе";
+		}
+		public function geneWoman()
+		{
+			 echo $this::geneticW;
+		}
+		public function geneChild()
+		{
+			echo $this->geneMan(); echo "  и  ";
+			echo $this->geneWoman();
 		}
 
 	}
-	class Child implements interfaceFarther, interfaceMother, familyInterface
-	{
-		public $age = 4;
-		public $sex = female;
 
-	}
-
+	$farther = new farther;
+	$mother = new mother;
+	$child = new child;
+	echo $farther->age = 27; echo "<br>";
+	echo $farther->type; echo "<br>";
+	echo $farther-> makeMoney(); echo "<br>";
+	echo $mother-> cook(); echo "<br>";	
+	echo $farther -> teachChild(); echo "<br>";
+	echo $mother -> loveEachOther(); echo "<br>";
+	echo $child -> makeMoney(); echo "<br>";
+	echo $child -> sex = "Женский пол"; echo "<br>";
+	echo $child -> peopleCount = 3; echo "<br>";
+	echo $mother -> geneWoman(); echo "<br>";
+	echo $farther -> geneMan(); echo "<br>";
+	echo $child-> teach(); echo "<br>";
+	echo $child -> geneChild(); echo "<br>";
+	
 
  ?>
